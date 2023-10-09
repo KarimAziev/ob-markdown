cc38763ab3a993e6dbece99688705269e1e1f3e3
KarimAziev <karim.aziiev@gmail.com>
2023-10-09 14:50:00 +0300

    feat: enhance `ob-markdown.el` with pandoc support
    This commit introduces significant enhancements to the `ob-markdown.el` script. It provides a way to strip properties from `org-mode` markdown previews with the `ob-markdown-preview-strip-properties` function. In addition, default header arguments for markdown are redefined to use the `pandoc` `--top-level-division=part` command, effectively introducing `pandoc` support. The `org-babel-execute:markdown` and `org-babel-expand-body:markdown` functions were updated to support the new changes.

--- ob-markdown.el
+++ ob-markdown.el
@@ -35,27 +35,90 @@
 ;;; Code:
 (require 'ob)
 (require 'ob-eval)
-(require 'markdown-mode)
+
 ;; possibly require modes required for your language
 
 ;; optionally define a file extension for this language
 (add-to-list 'org-babel-tangle-lang-exts '("markdown" . "text"))
 
+
+(defun ob-markdown-preview-strip-propererties ()
+  "Remove properties from `org-mode' markdown preview."
+  (save-excursion
+    (goto-char (point-min))
+    (let ((case-fold-search t))
+      (while (re-search-forward
+              ":PROPERTIES:[\n]+[\s\t]+:CUSTOM_ID:[\s\t][^\n]+[\n]+[\s\t]+:END:[\n]"
+              nil t
+              1)
+        (replace-match "")))))
+
 ;; optionally declare default header arguments for this language
-(defvar org-babel-default-header-args:markdown '())
+(defvar org-babel-default-header-args:markdown '((:cmdline . "--top-level-division=part")))
+(defvar org-babel-command:markdown "pandoc"
+  "Command run by ob-markdown.")
+
+
+(defun ob-markdown-pandoc-from-string (string input-type output-type &rest args)
+  "Execute `pandoc' on STRING in INPUT-TYPE to OUTPUT-TYPE."
+  (let ((args (delq nil
+                    (append
+                     (and input-type
+                          (list "-f" input-type))
+                     (and output-type
+                          (list "-t" output-type))
+                     args)))
+        (exit-code))
+    (with-temp-buffer
+      (insert string)
+      (setq exit-code (apply #'call-process-region (append (list (point-min)
+                                                                 (point-max))
+                                                           (list "pandoc" t t nil)
+                                                           args)))
+      (cond ((zerop exit-code)
+             (pcase output-type
+               ("org" (save-excursion
+                        (goto-char (point-min))
+                        (let ((case-fold-search t))
+                          (while (re-search-forward
+                                  ":PROPERTIES:[\n]+[\s\t]+:CUSTOM_ID:[\s\t][^\n]+[\n]+[\s\t]+:END:[\n]"
+                                  nil t
+                                  1)
+                            (replace-match ""))))))
+             (buffer-string))
+            (t
+             (let ((error-buffer (get-buffer-create " *Org-Babel Error*"))
+                   (stderr (buffer-string)))
+               (if (or (not (numberp exit-code))
+                       (> exit-code 0)
+                       (not (string-empty-p stderr)))
+                   (progn
+                     (org-babel-eval-error-notify exit-code stderr)
+                     (buffer-string))
+                 (buffer-string))))))))
+
 
 ;; This function expands the body of a source code block by doing
 ;; things like prepending argument definitions to the body, it should
 ;; be called by the `org-babel-execute:markdown' function below.
 (defun org-babel-expand-body:markdown (body params &optional processed-params)
   "Expand BODY according to PARAMS, return the expanded body."
-  body)
+  (org-babel-expand-body:generic body params))
 
 (defun org-babel-execute:markdown (body params)
   "Execute a block of Markdown code with org-babel.  This function is
 called by `org-babel-execute-src-block'."
   (message "executing Markdown source code block")
-  (org-babel-eval markdown-command (org-babel-expand-body:markdown body params)))
+  (let* ((cmdline (cdr (assoc :cmdline params)))
+         (cmdline (if cmdline (concat " " cmdline) ""))
+         (result-type (cdr (assq :result-params params)))
+         (out-type
+          (cond ((member "html" result-type)
+                 "html")
+                ((member "file" result-type)
+                 nil)
+                (t "org"))))
+    (ob-markdown-pandoc-from-string body "gfm" out-type)))
 
 ;; This function should be used to assign any variables in params in
 ;; the context of the session environment.
