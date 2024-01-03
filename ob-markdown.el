96bcda175c8bccefff36b26cddefc2fccb76f6a1
KarimAziev <karim.aziiev@gmail.com>
2024-01-03 15:19:37 +0200

    feat: enhance Markdown support to use pandoc and args
    - Update default header arguments to use GitHub Flavored Markdown
      (GFM) format.
    - Refactor pandoc execution to use a new helper function
      `ob-markdown-babel-eval` for better error handling and output
      processing.
    - Remove the obsolete `ob-markdown-preview-strip-propererties`
      function.
    - Add `ob-markdown-out-type-from-cmdline` and
      `ob-markdown-get-arg-value` functions to parse command line
      arguments and extract output types or other values.
    - Modify `org-babel-execute:markdown` to use the new helper
      functions and support additional output types based on command
      line arguments.
    - Ensure compatibility with org-babel's error notification system.
    - Clean up and improve code documentation for better readability
      and maintenance.

--- ob-markdown.el
+++ ob-markdown.el
@@ -44,89 +44,123 @@
 ;; optionally define a file extension for this language
 (add-to-list 'org-babel-tangle-lang-exts '("markdown" . "text"))
 
-
-(defun ob-markdown-preview-strip-propererties ()
-  "Remove properties from `org-mode' markdown preview."
-  (save-excursion
-    (goto-char (point-min))
-    (let ((case-fold-search t))
-      (while (re-search-forward
-              ":PROPERTIES:[\n]+[\s\t]+:CUSTOM_ID:[\s\t][^\n]+[\n]+[\s\t]+:END:[\n]"
-              nil t
-              1)
-        (replace-match "")))))
-
 ;; optionally declare default header arguments for this language
-(defvar org-babel-default-header-args:markdown '((:cmdline . "--top-level-division=part")
+(defvar org-babel-default-header-args:markdown '((:cmdline . "-f gfm")
                                                  (:results . "raw")))
 (defvar org-babel-command:markdown "pandoc"
   "Command run by ob-markdown.")
 
 
-(defun ob-markdown-pandoc-from-string (string input-type output-type &rest args)
-  "Execute `pandoc' on STRING in INPUT-TYPE to OUTPUT-TYPE."
-  (let ((args (delq nil
-                    (append
-                     (and input-type
-                          (list "-f" input-type))
-                     (and output-type
-                          (list "-t" output-type))
-                     args)))
-        (exit-code))
-    (with-temp-buffer
-      (insert string)
-      (setq exit-code (apply #'call-process-region (append (list (point-min)
-                                                                 (point-max))
-                                                           (list org-babel-command:markdown t t nil)
-                                                           args)))
-      (cond ((zerop exit-code)
-             (pcase output-type
-               ("org" (save-excursion
-                        (goto-char (point-min))
-                        (let ((case-fold-search t))
-                          (while (re-search-forward
-                                  ":PROPERTIES:[\n]+[\s\t]+:CUSTOM_ID:[\s\t][^\n]+[\n]+[\s\t]+:END:[\n]"
-                                  nil t
-                                  1)
-                            (replace-match ""))))))
-             (buffer-string))
-            (t
-             (let ((error-buffer (get-buffer-create " *Org-Babel Error*"))
-                   (stderr (buffer-string)))
-               (if (or (not (numberp exit-code))
-                       (> exit-code 0)
-                       (not (string-empty-p stderr)))
-                   (progn
-                     (org-babel-eval-error-notify exit-code stderr)
-                     (buffer-string))
-                 (buffer-string))))))))
-
-
 ;; This function expands the body of a source code block by doing
 ;; things like prepending argument definitions to the body, it should
 ;; be called by the `org-babel-execute:markdown' function below.
-(defun org-babel-expand-body:markdown (body params &optional processed-params)
+(defun org-babel-expand-body:markdown (body params &optional _processed-params)
   "Expand BODY according to PARAMS, return the expanded body."
   (org-babel-expand-body:generic body params))
+
+(defun ob-markdown-babel-eval (command query)
+  "Run COMMAND on QUERY.
+
+Return standard output produced by COMMAND.  If COMMAND exits
+with a non-zero code or produces error output, show it with
+`org-babel-eval-error-notify'.
+
+Writes QUERY into a temp-buffer that is processed with
+`org-babel--shell-command-on-region'."
+  (let ((error-buffer (get-buffer-create " *Org-Babel Error*"))
+        exit-code)
+    (with-current-buffer error-buffer (erase-buffer))
+    (with-temp-buffer
+      ;; Ensure trailing newline.  It is required for cmdproxy.exe.
+      (insert query "\n")
+      (setq exit-code
+            (org-babel--shell-command-on-region
+             command error-buffer))
+      (let ((stderr (with-current-buffer error-buffer (buffer-string))))
+        (if (or (not (numberp exit-code))
+                (> exit-code 0)
+                (not (string-empty-p stderr)))
+            (progn
+              (org-babel-eval-error-notify exit-code stderr)
+              (save-excursion
+                (when (get-buffer org-babel-error-buffer-name)
+                  (with-current-buffer org-babel-error-buffer-name
+                    (unless (derived-mode-p 'compilation-mode)
+                      (compilation-mode))
+                    ;; Compilation-mode enforces read-only, but
+                    ;; Babel expects the buffer modifiable.
+                    (setq buffer-read-only nil))))
+              ;; Return output, if any.
+              (buffer-string))
+          (pcase (ob-markdown-out-type-from-cmdline command)
+            ("org" (save-excursion
+                     (goto-char (point-min))
+                     (let ((case-fold-search t))
+                       (while (re-search-forward
+                               ":PROPERTIES:[\n]+[\s\t]+:CUSTOM_ID:[\s\t][^\n]+[\n]+[\s\t]+:END:[\n]"
+                               nil t
+                               1)
+                         (replace-match ""))))))
+          (buffer-string))))))
+
+
+(defun ob-markdown-out-type-from-cmdline (cmdline)
+  "Extract output type from CMDLINE using predefined flags.
+
+Argument CMDLINE is a string containing the command line to be parsed."
+  (ob-markdown-get-arg-value cmdline
+                             "-t" "-w" "--to" "--write"))
+
+(defun ob-markdown-get-arg-value (cmdline &rest args)
+  "Extract argument value from a command line string.
+
+Argument CMDLINE is a string representing the command line to be parsed.
+
+Remaining arguments ARGS are strings representing the arguments to search for in
+CMDLINE."
+  (with-temp-buffer (insert cmdline)
+                    (goto-char (point-min))
+                    (when (re-search-forward (regexp-opt args) nil t 1)
+                      (when (looking-at "[\s=]")
+                        (forward-char 1)
+                        (when (re-search-forward
+                               "[a-z]+"
+                               nil t
+                               1)
+                          (match-string-no-properties 0))))))
+
 
 (defun org-babel-execute:markdown (body params)
   "Execute a block of Markdown code with org-babel.  This function is
 called by `org-babel-execute-src-block'."
   (message "executing Markdown source code block")
-  (let* ((cmdline (cdr (assoc :cmdline params)))
-         (cmdline (if cmdline (concat " " cmdline) ""))
-         (result-type (cdr (assq :result-params params)))
-         (out-type
-          (cond ((member "html" result-type)
-                 "html")
-                ((member "file" result-type)
-                 nil)
-                (t "org"))))
-    (ob-markdown-pandoc-from-string body "gfm" out-type)))
+  (let* ((expanded-body (org-babel-expand-body:markdown body params))
+         (cmdline (cdr (assoc :cmdline params)))
+         (outtype-from-args (and cmdline
+                                 (ob-markdown-out-type-from-cmdline cmdline)))
+         (result-type (cdr (assq :result-params params))))
+    (cond ((and cmdline
+                (ob-markdown-get-arg-value cmdline "-f" "--from")))
+          (t
+           (setq cmdline (string-join (delq nil (list "-f" "gfm" cmdline)) " "))))
+    (cond (outtype-from-args)
+          ((member "html" result-type)
+           (setq cmdline (string-join (delq nil (list "-t" "html" cmdline)) " ")))
+          ((member "file" result-type)
+           nil)
+          (t
+           (setq cmdline (string-join (delq nil (list "-t" "org" cmdline)) " "))))
+    (ob-markdown-babel-eval (if cmdline
+                                (concat org-babel-command:markdown
+                                        " "
+                                        cmdline)
+                              org-babel-command:markdown)
+                            expanded-body)))
+
 
 ;; This function should be used to assign any variables in params in
 ;; the context of the session environment.
-(defun org-babel-prep-session:markdown (session params)
+(defun org-babel-prep-session:markdown (_session _params)
   "Return an error if the :session header argument is Set.
 Markdown does not support sessions."
   (error "Markdown sessions are nonsensical."))
